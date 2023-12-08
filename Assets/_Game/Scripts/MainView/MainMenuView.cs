abf905f2744b9e7d443b5c97874981782e94af55
Pedro Mendes <pedrohenrimp@gmail.com>
2023-12-08 10:11:30 -0300

    update menus

--- Assets/_Game/Scripts/MainView/MainMenuView.cs
+++ Assets/_Game/Scripts/MainView/MainMenuView.cs
@@ -1,17 +1,31 @@
 using Biorama.Essentials;
 using Biorama.Popups;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Biorama.View
 {
     public class MainMenuView : MonoBehaviour
     {
+        [SerializeField]
+        [CustomName("Continue Button")]
+        private Button mContinueButton;
+
+        private void OnEnable()
+        {
+            //mContinueButton.interactable = ServiceLocator.Instance.PlayerGameData.HasSavedGame;
+        }
         public void OnSettingsButtonClicked()
         {
             SettingsPopup.Instance.Show();
         }
 
-        public void OnPlayButtonClicked()
+        public void OnNewGameButtonClicked()
+        {
+            ServiceLocator.Instance.CustomSceneManager.LoadScene(SceneType.PlayScene);
+        }
+
+        public void OnContinueButtonClicked()
         {
             ServiceLocator.Instance.CustomSceneManager.LoadScene(SceneType.PlayScene);
         }
--- Assets/_Game/Scripts/Popup/SettingsPopup.cs
+++ Assets/_Game/Scripts/Popup/SettingsPopup.cs
@@ -1,30 +1,100 @@
-using System;
+using Biorama.Essentials;
+using Biorama.Popups.Book;
+using Biorama.ScriptableAssets.Book;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Biorama.Popups
 {
+    public enum OptionsType
+    {
+        MainOptions,
+        InGameOptions
+    }
     public class SettingsPopup : BasePopup<SettingsPopup>
     {
         private static readonly string PopupName = nameof(SettingsPopup);
 
         public static SettingsPopup Instance => GetPopup(PopupName);
 
-        private Action mOnConfirmationCallback;
+        #region Members
+        [SerializeField]
+        [CustomName("Audio Plus Button")]
+        private Button mAudioPlusButton;
 
-        public void OnConfirmationClicked()
+        [SerializeField]
+        [CustomName("Audio Minus Button")]
+        private Button mAudioMinusButton;
+
+        [SerializeField]
+        [CustomName("Home Button")]
+        private GameObject mHomeButton;
+
+        [SerializeField]
+        [CustomName("Hide Button")]
+        private GameObject mHideButton;
+        #endregion
+
+        #region Methods
+        public void Show(OptionsType aOptionsType = OptionsType.MainOptions)
         {
-            if(mOnConfirmationCallback != null)
-                mOnConfirmationCallback?.Invoke();
+            SetupCloseButtons(aOptionsType);
 
-            Debug.Log("Confirmation Clicked");
+            base.Show();  
+        }
+
+        public override void Hide()
+        {
+            base.Hide();
+        }
+
+        public void OnInfoButtonClicked()
+        {
+
+        }
+
+        public void OnControlsButtonClicked()
+        {
+
+        }
+
+        public void OnContactButtonClicked()
+        {
+
+        }
+
+        public void OnAudioButtonClicked()
+        {
+
+        }
+
+        public void OnAudioMinusButtonClicked()
+        {
+
+        }
+
+        public void OnAudioPlusButtonClicked()
+        {
+
+        }
+
+        public void OnHomeButtonClicked()
+        {
             Hide();
         }
 
-        public void SetOnConfirmationCallback(Action aAction)
+        public void OnCloseButtonClicked()
         {
-            mOnConfirmationCallback = aAction;
+            Hide();
         }
+
+        private void SetupCloseButtons(OptionsType aOptionsType)
+        {
+            mHomeButton.SetActive(aOptionsType == OptionsType.MainOptions);
+            mHideButton.SetActive(aOptionsType == OptionsType.InGameOptions);
+        }
+        #endregion
     }
 }
